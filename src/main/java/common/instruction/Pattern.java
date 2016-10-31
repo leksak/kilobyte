61f370407e934b3fdcff69181efb66dc1c29db20
Filip Allberg <filip@cs.umu.se>
2016-10-31 14:25:04 +0100

    Cleaning

--- src/main/java/common/instruction/Pattern.java
+++ src/main/java/common/instruction/Pattern.java
@@ -1,9 +1,5 @@
 package common.instruction;
 
-import com.google.common.base.Preconditions;
-
-import static com.google.common.base.Preconditions.checkArgument;
-
 public enum Pattern {
   INAME_RD_RS_RT(3),
   NOP(0),
@@ -13,10 +9,6 @@ public enum Pattern {
 
   Pattern(int expectedNumberOfArguments) {
     this.expectedNumberOfArguments = expectedNumberOfArguments;
-  }
-
-  boolean correctNumberOfArguments(String... args) {
-    return correctNumberOfArguments(args.length);
   }
 
   boolean correctNumberOfArguments(int actualNumberOfArguments) {
