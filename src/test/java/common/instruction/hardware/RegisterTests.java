de2ba39e658a630df75df6de9c2228b2c4c231cf
Filip Allberg <filip@cs.umu.se>
2016-11-02 17:14:31 +0100

    Regression tests

---
+++ src/test/java/common/instruction/hardware/RegisterTests.java
@@ -0,0 +1,22 @@
+package common.instruction.hardware;
+
+import common.hardware.Register;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RegisterTests {
+  @Test
+  void testThatSymbolicAndNonSymbolicRepresentationsAreEquivalent() {
+    assertEquals(Register.fromString("$t1"), Register.fromString("$9"));
+  }
+
+  @Test
+  void testThatFromStringYieldsCorrectRegisterInstance() {
+    assertAll("equals",
+          () -> assertEquals(Register.$t1, Register.fromString("$t1")),
+          () -> assertEquals(Register.$t1, Register.fromString("$9"))
+    );
+  }
+}
