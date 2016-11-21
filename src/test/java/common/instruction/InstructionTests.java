75b2e3f31da4c6767c8543eb050489cefe4f54fc
Filip Allberg <filip@cs.umu.se>
2016-11-21 14:19:56 +0100

    Found some data that breaks things

--- src/test/java/common/instruction/InstructionTests.java
+++ src/test/java/common/instruction/InstructionTests.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static common.instruction.Format.I;
+import static common.instruction.Format.J;
+import static common.instruction.Format.R;
+import static common.instruction.Type.B;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -139,5 +143,37 @@ class InstructionTests {
 
       assertThat(instructionNeg, is(not(equalTo(instructionPos))));
     }
+  }
+
+  @Nested
+  @DisplayName("Tests checking that our output matches the test-data we've been supplied")
+  class TestsAgainstSuppliedTestData {
+    Object[][] givenTestData = {
+          { 0x23bdfff8,  I, "[8 29 29 65528]", "[8 0x1d 0x1d 0xfff8]", "addi $sp, $sp, -8"},
+          { 0xafbf0004,  I, "[43 29 31 4]",    "[0x2b 0x1d 0x1f 4]",   "sw $ra, 4($sp)"},
+          { 0xafa40000,  I,  "[43 29 4 0]",    "[0x2b 0x1d 4 0]",      "sw $a0, 0($sp)"},
+          //TODO:{ 0x11000003,  B,  "[4 8 0 3]",      "[4 8 0 3]",            "beq $t0, $zero, 4"},
+          { 0x28880001,  I,  "[10 4 8 1]",     "[0xa 4 8 1]",          "slti $t0, $a0, 1"},
+          //TODO:{ 0x11000003,  B, "[4 8 0 3]",       "[4 8 0 3]",            "beq $t0, $zero, 4"},
+          { 0x20020001,  I, "[8 0 2 1]",       "[8 0 2 1]",            "addi $v0, $zero, 1"},
+          { 0x23bd0008,  I, "[8 29 29 8]",     "[8 0x1d 0x1d 8]",      "addi $sp, $sp, 8"},
+          { 0x03e00008,  J, "[0 31 0 0 8]",    "[0 0x1f 0 0 8]",       "jr $ra"},
+          { 0x2084ffff,  I, "[8 4 4 65535]",   "[8 4 4 0xffff]",       "addi $a0, $a0, -1"},
+          //TODO:{ 0x0c100000,  J,  "[3 1048576]",    "[3 0x100000]",         "jal 0x00400000"},
+          { 0x8fa40000,  I, "[35 29 4 0]",     "[0x23 0x1d 4 0]",      "lw $a0, 0($sp)"},
+          { 0x8fbf0004,  I, "[35 29 31 4]",    "[0x23 0x1d 0x1f 4]",   "lw $ra, 4($sp)"},
+          { 0x23bd0008,  I, "[8 29 29 8]",     "[8 0x1d 0x1d 8]",      "addi $sp, $sp, 8"},
+          { 0x70821002,  R,  "[28 4 2 2 0 2]", "[0x1c 4 2 2 0 2]",     "mul $v0, $a0, $v0"},
+          { 0x03e00008,  J, "[0 31 0 0 8]",    "[0 0x1f 0 0 8]",       "jr $ra"},
+          //{ 0x00012122,  R, "[0 0 1 4 4 34]",  "[0 0 1 4 4 0x22]",     "sub $a0, $zero, $at"},
+    };
+
+    @Test
+    void testGivenTestData() throws NoSuchInstructionException {
+      for (Object[] o : givenTestData) {
+        assertThat(Instruction.unsafeFrom((long) (int) o[0]), is(equalTo(Instruction.from((String) o[4]))));
+      }
+    }
+
   }
 }
